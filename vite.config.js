b6be9355f8b8be6c198faa80b85f84c00ed9c37f
Tarachand-Gupta <tarachandragupta2784@gmail.com>
2022-12-14 19:00:45 +0530

    micro frontend config done

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    new federation({
+      name: "productApp",
+      filename: "remoteEntry.js",
+      remotes: {},
+      exposes: {
+        "./ProductApp": "./src/productApp/ProductApp",
+      },
+      shared: ["react"],
+    }),
+  ],
+  preview: {
+    host: "localhost",
+    port: 5173,
+    strictPort: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  },
+  build: {
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
